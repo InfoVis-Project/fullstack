5e9d4ed9c58f9e96fe2bcb7f3badbb0d6de2159e
erdoganabaci <erdoganabaci97@gmail.com>
2022-05-24 14:00:31 +0200

    Implement Graphs with new versions

--- frontend/src/layouts/dashboard-producer/index.js
+++ frontend/src/layouts/dashboard-producer/index.js
@@ -13,8 +13,11 @@ import Footer from "examples/Footer";
 import BiaxialLineChart from "examples/Charts/Recharts/BiaxialLineChart";
 import BiaxialLineChartGraph1 from "examples/Charts/Recharts/NewVersion/Graph1";
 import VerticalComposedChart1 from "examples/Charts/Recharts/NewVersion/Graph2";
+import VerticalComposedChart3 from "examples/Charts/Recharts/NewVersion/Graph3";
+import SimpleScatterChart from "examples/Charts/Recharts/NewVersion/Graph4";
 import SimpleAverageBarChart from "examples/Charts/Recharts/SimpleAverageBarChart";
 import SimpleActingResumeBarChart from "examples/Charts/Recharts/SimpleActingResumeBarChart";
+
 // Dashboard components
 // import Projects from "layouts/dashboard/components/Projects";
 // import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
@@ -49,10 +52,32 @@ function DashboardProducer() {
 
             <Grid item xs={6} md={6} lg={6}>
               <MDBox mb={6}>
+                <VerticalComposedChart3
+                  color="light"
+                  title="Popular directors of the year"
+                  description="Graph VerticalComposedChart"
+                  date="last update 2 days ago"
+                />
+              </MDBox>
+            </Grid>
+
+            <Grid item xs={6} md={6} lg={6}>
+              <MDBox mb={6}>
                 <BiaxialLineChartGraph1
                   color="light"
                   title="New Version Movie/Series genre ranking per year"
                   description="Graph BiaxialLineChart"
+                  date="last update 2 days ago"
+                />
+              </MDBox>
+            </Grid>
+
+            <Grid item xs={6} md={6} lg={6}>
+              <MDBox mb={6}>
+                <SimpleScatterChart
+                  color="light"
+                  title="Box office contribution"
+                  description="Graph SimpleScatterChart"
                   date="last update 2 days ago"
                 />
               </MDBox>
